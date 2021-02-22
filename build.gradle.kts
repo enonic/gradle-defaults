56b857b2ea39c3efa29f6c23055b25db2802e007
rymsha <rymsha@yandex.ru>
2021-02-22 16:30:05 +0100

    Get ready to Gradle 7 #14

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    groovy
+    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.13.0"
+}
+
+version = "2.0.2-SNAPSHOT"
+group = "com.enonic.gradle"
+
+gradlePlugin {
+    plugins {
+        register("defaults_plugin") {
+            id = "com.enonic.defaults"
+            implementationClass = "com.enonic.defaults.DefaultsPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://enonic.com"
+    vcsUrl = "https://github.com/enonic/gradle-defaults"
+    (plugins) {
+        "defaults_plugin" {
+            displayName = "Enonic Defaults Plugin"
+            description = "Defaults plugin for Enonic projects."
+            tags = listOf("enonic", "java", "groovy")
+        }
+    }
+}
